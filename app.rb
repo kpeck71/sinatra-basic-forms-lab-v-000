60010d9331f8b4677cbbd7f0eb94f471808a3367
Kristen Peck <kpeck71@gmail.com>
2017-12-09 16:43:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
   get '/' do
@@ -12,7 +12,7 @@ class App < Sinatra::Base
 
   post '/display_puppy' do
     @puppy = Puppy.new(params[:name],params[:breed],params[:age])
-    
+
     erb :display_puppy
   end
 end
